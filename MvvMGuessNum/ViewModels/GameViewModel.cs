a24ac04a8c4c61eb5f8cd1173aa47726d118f368
Student <Student@DESKTOP-FPC590P>
2026-02-26 15:45:23 +0300

    correct

--- MvvMGuessNum/ViewModels/GameViewModel.cs
+++ MvvMGuessNum/ViewModels/GameViewModel.cs
@@ -11,7 +11,7 @@ namespace MvvMGuessNum.ViewModels
     {
         public GameViewModel()
         {
-            GuessCommand = new RelayCommand(GuessNum);
+            GuessCommand = new RelayCommand(GuessNum, CanGuess);
             NewGameCommand = new RelayCommand(NewGame);
         }
 
@@ -66,7 +66,7 @@ namespace MvvMGuessNum.ViewModels
                 field = value;
                 OnPropertyChanged(nameof(Popitka));
             }
-        } = 5;
+        } = 10;
 
         private void NewGame(object parametr)
         {
